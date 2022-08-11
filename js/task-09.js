3b2942f01233f0356d6c12ed741e5ac19793c73a
DenisKhrebtov <khrebtovdenis@gmail.com>
2022-08-11 14:54:07 +0300

    finished hw6

--- js/task-09.js
+++ js/task-09.js
@@ -2,14 +2,12 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const colorEl = document.querySelector('color');
-const butnEl = document.querySelector('change-color');
-const bodyEl = document.body;
+const colorEl = document.querySelector('.color');
+const butnEl = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
 
-butEl.addEventListener('click', onButnClick);
-
-function onButnClick(event) {
-  if () {
-    
-  }
-}
+butnEl.addEventListener('click', function () {
+  let fu = getRandomHexColor();
+  bodyEl.style.backgroundColor = fu;
+  colorEl.textContent = fu;
+});
